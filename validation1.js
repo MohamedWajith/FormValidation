b80013b40537f43ec7c09ca6bc8d23fb53a8cae9
MohamedWajith <wajith_wajith@yahoo.com>
2022-11-25 00:01:56 +0530

    validation 1 completed

--- validation1.js
+++ validation1.js
@@ -1,81 +1,66 @@
 function checkValidation(){
-            
-            var nic=document.getElementById("nic");
-            var mno=document.getElementById("mno");
-            var email=document.getElementById("email");
-            var pwd=document.getElementById("pwd");
-            var pwd2=document.getElementById("pwd2");
-            var name=document.getElementById("name");
-            var photo=document.getElementById("photo");
+        var name=document.getElementById("name");
+        var patname=/^[a-zA-z]+$/;
     
-    //console.log("Name : "+name);  
-            
-            var patnic=/^[0-9]{9}[vVxX]$/;
-            var patnewnic=/^[0-9]{12}$/;
-            var patname=/^[a-zA-Z]+$/;
-            var patemail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            var patphototo=/(\.jpg|\.jpeg|\.png|\.gif)$/i;
-            var patm=/^07[0-9]{8}$/;
-            
-            
-            //validation 
-         /*   if(!name.value.match(patname)){
-                    document.getElementById("errorname").innerHTML="*Invalid Name";
-                    document.getElementById("errorname").style.color="red";
-                    name.focus();
-                    name.select();
-                    return false;  
-            }
-            
+        if(!name.value.match(patname)){
+            document.getElementById("errorname").innerHTML="*Invalid Name";
+            document.getElementById("errorname").style.color="red";
+            return false;
+        }
+        
+        var email=document.getElementById("email"); 
+        var patemail=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
             if(!email.value.match(patemail)){
-                    document.getElementById("erroremail").innerHTML="*Invalid Email";
-                    document.getElementById("erroremail").style.color="red";
-                    email.focus();
-                    email.select();
-                    return false;  
-            }
-            
-            if(!pwd.length > 8){
-                  //  document.getElementById("errorpwd").innerHTML="*Invalid Password";
-                  //  document.getElementById("errorpwd").style.color="red";
-                  //  pwd.focus();
-                  //  pwd.select();
-                  //  return false;
-                console.log(pwd);
-                
-            }
-            if(pwd!=pwd2){
-                 //   document.getElementById("errorpwd2").innerHTML="*Password doesn't match";
-                 //   document.getElementById("errorpwd2").style.color="red";
-                 //   pwd2.focus();
-                 //   pwd2.select();
-                   alert("Password Not Match!!!!");
-                   
-
-            } 
-            
-            
-            
-            
-            if(!nic.value.match(patnewnic)){
-               
-                    document.getElementById("errornic").innerHTML="*Invalid NIC Number";
-                    document.getElementById("errornic").style.color="red";
-                    nic.focus();
-                    nic.select();
-                    return false;
-            }
-            
-            
-            if(!mno.value.match(patm)){
-                    document.getElementById("errormno").innerHTML="*Invalid Mobile Number";
-                    document.getElementById("errormno").style.color="red";
-                    mno.focus();
-                    mno.select();
-                    document.getElementById("errormno").innerHTML="";
-                    return false;
+                   document.getElementById("erroremail").innerHTML="*Invalid Email";
+                   document.getElementById("erroremail").style.color="red";
+                   return false;     
+          }
+    
+        var pwd=document.getElementById("pwd");
+        var patpwd=/^.{8,}$/;
+        
+    
+            if(!pwd.value.match(patpwd)){
+                   document.getElementById("errorpwd").innerHTML="*Invalid Password";
+                   document.getElementById("errorpwd").style.color="red";
+                   return false;
                }
-            */
+        var pwd=document.getElementById("pwd").value;
+        var pwd2=document.getElementById("pwd2").value;
+                if (pwd != pwd2) {
+                   document.getElementById("errorpwd2").innerHTML="*Invalid Password";
+                   document.getElementById("errorpwd2").style.color="red";
+                return false;
+        }
+    
+        var nic=document.getElementById("nic");
+        var patnic=/^[0-9]{9}[vVxX]$/;
+        var patnicnew=/^[0-9]{12}$/;
+
+                if(!nic.value.match(patnic) && !nic.value.match(patnicnew)){
+                   document.getElementById("errornic").innerHTML="Invalid nic";
+                   document.getElementById("errornic").style.color="red";
+                   return false;      
+            
+             }
+        var mno=document.getElementById("mno");
+        var patmno=/^07[0-9]{8}$/;
+        
+                if(!mno.value.match(patmno)){
+                   document.getElementById("errormno").innerHTML="Invalid number";
+                   document.getElementById("errormno").style.color="red";
+                   return false;
+                }
+    
+        var photo=document.getElementById("photo");
+        var patphoto=/(\.jpg|\.jpeg|\.png|\.gif)$/;
+                
+                if(!photo.value.match(patphoto)){
+                   document.getElementById("errorphoto").innerHTML="Invalid Format";
+                   document.getElementById("errorphoto").style.color="red";
+                   return false;
+                }
+            
         }
         
         
